0f0e54b41446ae2b4520bbaae3d2ae0ace604a06
Xtremly <738725744@qq.com>
2024-12-01 07:52:22 +0800

    initial commit

---
+++ Promise/TypeConverters/TypeConverterExtensions.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Promise;
+
+/// <summary>
+/// a <see langword="class"/> of <see cref="TypeConverterExtensions"/>
+/// </summary>
+public static class TypeConverterExtensions
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private static readonly ConcurrentDictionary<Type, ConcurrentDictionary<Type, TypeConverter>> typeConvertMaps = new();
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private static readonly string[] typeCodeNames = Enum.GetNames(typeof(TypeCode));
+
+    /// <summary>
+    /// convert <see cref="object"/> to <typeparamref name="To"/>
+    /// </summary>
+    /// <typeparam name="To"></typeparam>
+    /// <param name="from"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidCastException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static To ConvertTo<To>(this object? from)
+    {
+        if (from is To to)
+        {
+            return to;
+        }
+
+        if (ConvertTo(from, typeof(To)) is To toValue)
+        {
+            return toValue;
+        }
+
+        throw new InvalidCastException("type conversion unsuccessful");
+    }
+
+    /// <summary>
+    /// convert <see cref="object"/> to  <paramref name="toType"/> />
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="toType"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidCastException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static object ConvertTo(this object? from, Type toType)
+    {
+        _ = from ?? throw new ArgumentNullException(nameof(from));
+
+        if (from is IConvertible convertible && typeCodeNames.Contains(toType.Name))
+        {
+            var converted = convertible.ToType(toType, CultureInfo.CurrentCulture);
+
+            if (converted is not null)
+            {
+                return converted;
+            }
+        }
+
+        Type fromType = from.GetType();
+
+        if (typeConvertMaps.TryGetValue(fromType, out ConcurrentDictionary<Type, TypeConverter>? fromTypeConverterStorages) == false)
+        {
+            typeConvertMaps[fromType] = fromTypeConverterStorages = new ConcurrentDictionary<Type, TypeConverter>();
+        }
+
+        if (fromTypeConverterStorages.TryGetValue(toType, out TypeConverter? toTypeConverter) == false)
+        {
+            toTypeConverter = TypeDescriptor.GetConverter(toType) ?? throw new InvalidOperationException("type converter not registered");
+
+            fromTypeConverterStorages[toType] = toTypeConverter;
+        }
+
+        if (toTypeConverter.CanConvertFrom(fromType))
+        {
+            return toTypeConverter.ConvertFrom(from)!;
+        }
+
+        throw new InvalidOperationException($"type converter from {fromType} to {toType} not registered");
+    }
+
+    /// <summary>
+    /// appen custom converter
+    /// </summary>
+    /// <typeparam name="TFrom"></typeparam>
+    /// <typeparam name="TTo"></typeparam>
+    /// <param name="converter"></param>
+    public static void AppendConverter<TFrom, TTo>(Func<TFrom, TTo> converter)
+    {
+        var fromType = typeof(TFrom);
+        var toType = typeof(TTo);
+
+        if (typeConvertMaps.TryGetValue(fromType, out ConcurrentDictionary<Type, TypeConverter>? targetTypeConverterMaps) == false)
+        {
+            typeConvertMaps[fromType] = targetTypeConverterMaps = new ConcurrentDictionary<Type, TypeConverter>();
+        }
+
+        targetTypeConverterMaps[toType] = new CustomTypeConverter<TFrom, TTo>(converter);
+    }
+
+    private class CustomTypeConverter<TFrom, TTo> : TypeConverter
+    {
+        Func<TFrom, TTo> converter;
+        static Type FromType = typeof(TFrom);
+        static Type ToType = typeof(TTo);
+
+        public CustomTypeConverter(Func<TFrom, TTo> converter)
+        {
+            this.converter = converter;
+        }
+
+        public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+        {
+            if (sourceType == FromType)
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+        {
+            if (value is TFrom from)
+            {
+                return converter(from);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ Promise/Utils/EnumDescriptor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.BindingFlags;
+
+namespace Promise;
+
+/// <summary>
+/// <see cref="Enum"/> analyzer
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public static class EnumDescriptor<T>
+    where T : struct, Enum
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    static EnumAttribute[] enumAttributes = Array.Empty<EnumAttribute>();
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    static EnumDescriptor()
+    {
+        if (typeof(T).IsEnum == false)
+        {
+            throw new ArgumentException("T must be an enumerated type");
+        }
+
+        var values = Enum.GetValues(typeof(T)).OfType<T>().ToArray();
+
+        Values = new ReadOnlyCollection<T>(values);
+
+        var fieldInfos = typeof(T).GetFields(Public | Static).Where(i => i.IsStatic).ToArray();
+
+        FieldInfos = new ReadOnlyCollection<FieldInfo>(fieldInfos);
+
+        Names = new ReadOnlyCollection<string>(fieldInfos.Select(i => i.Name).ToArray());
+
+        enumAttributes = new EnumAttribute[fieldInfos.Length];
+
+        for (int i = 0; i < fieldInfos.Length; i++)
+        {
+            var fieldInfo = fieldInfos[i];
+
+            var value = (T)fieldInfo.GetValue(null)!;
+
+            var attributes = fieldInfo.GetCustomAttributes().ToArray();
+
+            var enumAttribute = new EnumAttribute(value, attributes);
+
+            enumAttributes[i] = enumAttribute;
+        }
+    }
+
+    /// <summary>
+    /// all values
+    /// </summary>
+    public static readonly IReadOnlyList<T> Values;
+
+    /// <summary>
+    /// all fieldInfos
+    /// </summary>
+    public static readonly IReadOnlyList<FieldInfo> FieldInfos;
+
+    /// <summary>
+    /// all enum name
+    /// </summary>
+    public static readonly IReadOnlyList<string> Names;
+
+    /// <summary>
+    /// get target <see cref="Attribute"/> maps
+    /// </summary>
+    /// <typeparam name="Attribute"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<EnumAttribute<Attribute>> GetAttributes<Attribute>()
+        where Attribute : System.Attribute
+    {
+        for (int i = 0; i < enumAttributes.Length; i++)
+        {
+            for (int j = 0; j < enumAttributes[i].Attributes.Length; j++)
+            {
+                if (enumAttributes[i].Attributes is Attribute attribute)
+                {
+                    yield return new EnumAttribute<Attribute>(enumAttributes[i].Value, attribute);
+                    break;
+                }
+            }
+        }
+    }
+
+    record struct EnumAttribute(T Value, Attribute[] Attributes);
+
+    /// <summary>
+    /// enum attribute
+    /// </summary>
+    /// <typeparam name="TAttribute"></typeparam>
+    /// <param name="Value"></param>
+    /// <param name="Attribute"></param>
+    public record struct EnumAttribute<TAttribute>(T Value, TAttribute Attribute)
+        where TAttribute : Attribute;
+}
